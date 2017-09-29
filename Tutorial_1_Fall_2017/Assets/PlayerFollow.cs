6f4a3259a390f527d2e0e68721c49722c18c0d27
Axel <haxelegide@yahoo.fr>
2017-09-29 10:12:54 -0400

    prefabs added as wall

---
+++ Tutorial_1_Fall_2017/Assets/PlayerFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform player;
+    public Transform cam;
+    public float cameraHeight = 18.0f;
+    private Vector3 currentPosition;
+
+    void Update () {
+        if (player)
+        {
+            currentPosition = new Vector3(player.position.x, cameraHeight, player.position.z);
+			cam.SetPositionAndRotation(currentPosition, Quaternion.Euler(90.0f, 0.0f, 0.0f));
+        }
+	}
+}
---
+++ Tutorial_1_Fall_2017/README.md
@@ -0,0 +1,2 @@
+# Tutorial_1_Fall_2017
+This repo, is to practice and make sure it works the tutorial mini-project works.
